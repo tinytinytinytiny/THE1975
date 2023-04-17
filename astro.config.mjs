c6da25ad8e6bb84830e25477c05cd247d2044ae8
tinytinytinytiny <tinynametaken@yahoo.com>
2023-04-16 21:53:54 -0400

    enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		assets: true
-	},
-	integrations: [tailwind(), mdx()]
+  experimental: {
+    assets: true
+  },
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: netlify()
 });
